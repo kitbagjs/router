41553949cf15dcdff161ef23fecaedcd4a356211
Craig Harshbarger <pleek91@gmail.com>
2023-11-21 16:04:58 -0500

    Initial repo setup

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/main.ts'),
+      name: '@kitbag/router',
+      fileName: 'kitbag-router',
+    },
+  },
+  plugins: [
+    dts({ 
+      rollupTypes: true 
+    })
+  ]
+})
