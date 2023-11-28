14b7526d20bfdb0f33a39c1e10e110474b234b30
Evan Sutherland <stackoverfloweth@gmail.com>
2023-11-28 08:28:15 -0600

    pausing to help Craig

---
+++ src/routeMatch.ts
@@ -0,0 +1,10 @@
+import { Routes } from "./routes";
+
+export type RouteRegex = {name: }
+export function routeMatch<T extends Routes>(routes: T): void {
+
+}
+
+// export function match<T extends Routes>(routes: T): T[number] {
+//   routes
+// }
