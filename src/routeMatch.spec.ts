fee9d1e038434eb45114cfc39596d1bb76f925a6
Evan Sutherland <stackoverfloweth@gmail.com>
2023-11-28 20:56:10 -0600

    built and tested removeLeadingAndTrailingSlashes

--- src/routeMatch.spec.ts
+++ src/routeMatch.spec.ts
@@ -1,5 +1,43 @@
-import { test } from 'vitest'
+import { describe, expect, test } from 'vitest'
+import {random} from './utilities'
+import { removeLeadingAndTrailingSlashes } from './routeMatch'
 
-test('test', () => {
-  
+describe('removeLeadingAndTrailingSlashes', () => {
+  test('given empty string, returns empty string', () => {
+    const input = ''
+
+    const response = removeLeadingAndTrailingSlashes(input)
+
+    expect(response).toBe('')
+  })
+
+  test('given string without leading or trailing slashes, returns same string', () => {
+    const input = 'ABC123'
+    
+    const response = removeLeadingAndTrailingSlashes(input)
+
+    expect(response).toBe(input)
+  })
+
+  test('given string with 1 or more leading slashes, returns same string without any leading slashes', () => {
+    const leading = '/'.repeat(random.number({min: 1, max: 5}))
+    const input = 'ABC123'
+    const inputWithLeading = `${leading}${input}`
+
+    console.log({inputWithLeading})
+    
+    const response = removeLeadingAndTrailingSlashes(inputWithLeading)
+
+    expect(response).toBe(input)
+  })
+
+  test('given string with 1 or more trailing slashes, returns same string without any trailing slashes', () => {
+    const trailing = '/'.repeat(random.number({min: 1, max: 5}))
+    const input = 'ABC123'
+    const inputWithTrailing = `${input}${trailing}`
+    
+    const response = removeLeadingAndTrailingSlashes(inputWithTrailing)
+
+    expect(response).toBe(input)
+  })
 })
--- src/routeMatch.ts
+++ src/routeMatch.ts
@@ -1,8 +1,44 @@
-import { Routes } from "./routes";
+import { Route, Routes } from "./routes";
 
-export type RouteRegex = {name: }
+export type RouteFlat = { name: string, regex: RegExp }
+
+export function flattenRoutes<T extends Routes>(routes: T, path = ''): RouteFlat[] {
+  return routes.reduce<RouteFlat[]>((value, route) => {
+    const routeRegex = generateRouteRegexPattern(path, route)
+
+    value.push({
+      name: route.name,
+      regex: new RegExp(routeRegex)
+    })
+
+    if(route.children) {
+      const flattened = flattenRoutes(route.children, routeRegex)
+      value = value.concat()
+    }
+
+    return value
+  },[])
+}
+
+export function generateRouteRegexPattern(path: string, route: Route): string {
+  const paramRegex = /(:[\w-]+)\W/g
+
+  throw 'not implemented'
+}
+
+export function combineRoute(...parts:string[]): string {
+  throw 'not implemented'
+}
+
+export function removeLeadingAndTrailingSlashes(value: string): string {
+  const regex = /^\/*(.*?)\/*$/g
+
+  const [, inside] = regex.exec(value) ?? []
+  return inside
+}
+
 export function routeMatch<T extends Routes>(routes: T): void {
-
+  throw 'not implemented'
 }
 
 // export function match<T extends Routes>(routes: T): T[number] {
