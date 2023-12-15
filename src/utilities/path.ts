cb7829f2f382f2878c96d992dfddac32b6fc3f5c
Craig Harshbarger <pleek91@gmail.com>
2023-12-14 20:38:36 -0600

    Update path implementation

--- src/utilities/path.ts
+++ src/utilities/path.ts
@@ -12,35 +12,34 @@ export type Path<
   P extends PathParams<T> = any
 > = {
   path: T,
-  params: P,
+  params: Required<P>,
+}
+
+function getParam<P extends Record<string, Param | undefined>>(params: P, param: string): Param {
+  return params[param] ?? String
 }
 
 export function path<T extends string, P extends PathParams<T>>(path: T, params: Identity<P>): Path<T, P> {
   const optionalParamRegex = /:\?([\w]+)(?=\W|$)/g
   const requiredParamRegex = /:([\w]+)(?=\W|$)/g
 
-  const value: Record<string, any> = params
+  const value: Record<string, Param> = {}
   let match
 
   while ((match = optionalParamRegex.exec(path)) !== null) {
     const [, paramName] = match
 
-    if (!(paramName in params)) {
-      value[paramName] = paramName in params ? optional(params[paramName]) : optional(String)
-    }
+    value[paramName] = optional(getParam(params, paramName))
   }
 
   while ((match = requiredParamRegex.exec(path)) !== null) {
     const [, paramName] = match
 
-
-    if (!(paramName in params)) {
-      value[paramName] = params[paramName] ?? String
-    }
+    value[paramName] = getParam(params, paramName)
   }
 
   return {
     path,
-    params: value,
+    params: value as Required<P>,
   }
 }
