a76cb1336dc29878c7aad99ca523ce4415f32eed
Evan Sutherland <stackoverfloweth@gmail.com>
2023-12-15 22:19:14 -0600

    Update src/types/optional.ts
    Co-authored-by: Craig Harshbarger <pleek91@gmail.com>

--- src/types/optional.ts
+++ src/types/optional.ts
@@ -1,7 +1,7 @@
 import { ExtractParamType, Param, ParamGetSet } from '@/types'
 import { getParamValue, setParamValue } from '@/utilities'
 
-export function optional<TParam extends Param, TValue extends ExtractParamType<TParam>>(param: TParam): ParamGetSet<TValue | undefined> {
+export function optional<TParam extends Param>(param: TParam): ParamGetSet<ExtractParamType<TParam> | undefined> {
   return {
     get: (value) => {
       if (value === '') {
