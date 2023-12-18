ece6a8d2c389bb6767b7a7183142f991e5028fd7
Evan Sutherland <stackoverfloweth@gmail.com>
2023-12-17 21:39:01 -0600

    basic poc for param validation

--- src/utilities/paramValidation.ts
+++ src/utilities/paramValidation.ts
@@ -1,11 +1,24 @@
-import { RouteFlat } from '@/types'
+import { Param, RouteFlat } from '@/types'
+import { getParamValue, findParamValues } from '@/utilities'
 
-export function routeParamsAreValid(path: string, route: RouteFlat): boolean {
+export function routeParamsAreValid(value: string, route: RouteFlat): boolean {
+  const entries = Object.entries<Param[]>(route.params)
 
-  // each param
-  // get string value from string path (param)
-  // run that through utilities.getParamValue (if non-simple param)
-  // if it throws exception, consider not swallowing
+  if (entries.length === 0) {
+    return true
+  }
+
+  for (const [key, paramsTuple] of entries) {
+    const stringValues = findParamValues(value, route.path, key)
+
+    try {
+      const parsed = stringValues.map((stringValue, index) => getParamValue(stringValue, paramsTuple[index]))
+
+      console.log(parsed)
+    } catch {
+      return false
+    }
+  }
 
   return true
 }
