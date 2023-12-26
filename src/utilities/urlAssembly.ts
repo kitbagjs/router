a1de294b03f038d134d7c507a45f9f55be18405c
Evan Sutherland <stackoverfloweth@gmail.com>
2023-12-26 10:11:00 -0600

    implemented url assembly utility

---
+++ src/utilities/urlAssembly.ts
@@ -0,0 +1,10 @@
+import { Param, Resolved, Route } from '@/types'
+import { setParamValuesOnUrl } from '@/utilities/paramsFinder'
+
+export function assembleUrl<T extends Resolved<Route>>(route: T, values: Record<string, unknown[]>): string {
+  const params = Object.entries<Param[]>(route.params)
+
+  return params.reduce((url, [name, params]) => {
+    return setParamValuesOnUrl(url, { name, params, values: values[name] })
+  }, route.path)
+}
