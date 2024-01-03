71164ec4129d6d399db44046a6bb5baf0029d427
Craig Harshbarger <pleek91@gmail.com>
2024-01-02 19:59:25 -0600

    Better test for error checking

--- src/utilities/updateBrowserUrl.spec.ts
+++ src/utilities/updateBrowserUrl.spec.ts
@@ -1,9 +1,6 @@
 import { expect, test } from 'vitest'
-import { isBrowser } from '@/utilities/isBrowser'
 import { updateBrowserUrl } from '@/utilities/updateBrowserUrl'
 
 test('does nothing when the window is not available', () => {
-  updateBrowserUrl('http://example.com2/foo')
-
-  expect(isBrowser).toBe(false)
+  expect(() => updateBrowserUrl('http://example.com2/foo')).not.toThrowError()
 })
