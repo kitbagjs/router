fb3e47d5ee4e98e18d300bfd090d9cea71ba9582
Evan Sutherland <stackoverfloweth@gmail.com>
2024-01-20 11:49:56 -0600

    add depth: number property to resolved routes

--- src/types/resolved.ts
+++ src/types/resolved.ts
@@ -5,6 +5,7 @@ export type Resolved<T extends Route> = {
   matched: T,
   matches: Route[],
   name: string,
+  depth: number,
   path: string,
   query: string,
   params: Record<string, Param[]>,
