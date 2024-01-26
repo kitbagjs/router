48132924e244a0037423ac683fec231b4613c00d
Craig Harshbarger <pleek91@gmail.com>
2024-01-25 21:09:31 -0600

    Name the test

--- src/utilities/createRouterPush.spec.ts
+++ src/utilities/createRouterPush.spec.ts
@@ -4,14 +4,13 @@ import { resolveRoutes } from '@/utilities/resolveRoutes'
 import { createRouterNavigation } from '@/utilities/routerNavigation'
 import { routes } from '@/utilities/testHelpers'
 
-test('', () => {
+test('push calls onLocationUpdated', () => {
   const onLocationUpdate = vi.fn()
   const navigation = createRouterNavigation({ onLocationUpdate })
   const resolved = resolveRoutes(routes)
   const push = createRouterPush<typeof routes>({ navigation, resolved })
 
   push({ route: 'parentA', params: { paramA: '' } })
-
 
   expect(onLocationUpdate).toHaveBeenCalledOnce()
 })
