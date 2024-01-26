c9797336a59a56ab2baa72947059e2723be7c0d1
Craig Harshbarger <pleek91@gmail.com>
2024-01-25 20:51:42 -0600

    Add some sample routes to the test utilities

--- src/utilities/testHelpers.ts
+++ src/utilities/testHelpers.ts
@@ -1,3 +1,5 @@
+import { Routes } from '@/types/routes'
+
 export const random = {
   number(options: { min?: number, max?: number } = {}): number {
     const { min, max } = { min: 0, max: 1, ...options }
@@ -8,3 +10,33 @@ export const random = {
 }
 
 export const component = { template: '<div>This is component</div>' }
+
+export const routes = [
+  {
+    name: 'parentA',
+    path: '/:paramA',
+    children: [
+      {
+        name: 'childA',
+        path: '/:?paramB',
+        children: [
+          {
+            name: 'grandChildA',
+            path: '/:paramC/:paramA',
+            component,
+          },
+        ],
+      },
+      {
+        name: 'childB',
+        path: '/:paramD',
+        component,
+      },
+    ],
+  },
+  {
+    name: 'parentB',
+    path: '/parentB',
+    component,
+  },
+] as const satisfies Routes
