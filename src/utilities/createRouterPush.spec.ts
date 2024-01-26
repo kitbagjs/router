9ea14f7fc7244dcb66901c919d0871cc6e7d2e14
Craig Harshbarger <pleek91@gmail.com>
2024-01-25 21:04:58 -0600

    Start writing tests

---
+++ src/utilities/createRouterPush.spec.ts
@@ -0,0 +1,17 @@
+import { expect, test, vi } from 'vitest'
+import { createRouterPush } from '@/utilities/createRouterPush'
+import { resolveRoutes } from '@/utilities/resolveRoutes'
+import { createRouterNavigation } from '@/utilities/routerNavigation'
+import { routes } from '@/utilities/testHelpers'
+
+test('', () => {
+  const onLocationUpdate = vi.fn()
+  const navigation = createRouterNavigation({ onLocationUpdate })
+  const resolved = resolveRoutes(routes)
+  const push = createRouterPush<typeof routes>({ navigation, resolved })
+
+  push({ route: 'parentA', params: { paramA: '' } })
+
+
+  expect(onLocationUpdate).toHaveBeenCalledOnce()
+})
