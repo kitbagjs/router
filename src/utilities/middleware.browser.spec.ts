fd5fb7a9c0661c51abd56bc9f80700b197f05294
Craig Harshbarger <pleek91@gmail.com>
2024-02-17 21:16:44 -0600

    simplify tests

--- src/utilities/middleware.browser.spec.ts
+++ src/utilities/middleware.browser.spec.ts
@@ -2,13 +2,14 @@ import { expect, test, vi } from 'vitest'
 import { RouteMiddleware } from '@/types'
 import { RouterPushError, RouterRejectionError, RouterReplaceError } from '@/types/errors'
 import { Route } from '@/types/routes'
+import { RouterRoute } from '@/utilities/createRouterRoute'
+import { createRouterRouteQuery } from '@/utilities/createRouterRouteQuery'
 import { executeMiddleware } from '@/utilities/middleware'
-import { resolveRoutes } from '@/utilities/resolveRoutes'
-import { getRouterRouteForUrl } from '@/utilities/routes'
 import { component } from '@/utilities/testHelpers'
 
 test('calls middleware with correct routes', () => {
   const middleware = vi.fn()
+
   const routeA = {
     name: 'routeA',
     path: '/routeA',
@@ -16,15 +17,27 @@ test('calls middleware with correct routes', () => {
     middleware,
   } as const satisfies Route
 
+  const routerRouteA: RouterRoute = {
+    matched: routeA,
+    matches: [routeA],
+    name: routeA.name,
+    query: createRouterRouteQuery(),
+    params: {},
+  }
+
   const routeB = {
     name: 'routeB',
     path: '/routeB',
     component,
   } as const satisfies Route
 
-  const resolved = resolveRoutes([routeA, routeB])
-  const routerRouteA = getRouterRouteForUrl(resolved, routeA.path)!
-  const routerRouteB = getRouterRouteForUrl(resolved, routeB.path)!
+  const routerRouteB: RouterRoute = {
+    matched: routeB,
+    matches: [routeB],
+    name: routeB.name,
+    query: createRouterRouteQuery(),
+    params: {},
+  }
 
   executeMiddleware({ to: routerRouteA, from: routerRouteB })
 
@@ -40,16 +53,22 @@ test.each<{ type: string, error: any, middleware: RouteMiddleware }>([
   { type: 'push', error: RouterPushError, middleware: (_to, { push }) => push('') },
   { type: 'replace', error: RouterReplaceError, middleware: (_to, { replace }) => replace('') },
 ])('throws exception when $type is called', async ({ error, middleware }) => {
-  const routeA = {
+  const route = {
     name: 'routeA',
     path: '/routeA',
     component,
     middleware,
   } as const satisfies Route
 
-  const resolved = resolveRoutes([routeA])
-  const routerRouteA = getRouterRouteForUrl(resolved, routeA.path)!
-  const execute = (): Promise<void> => executeMiddleware({ to: routerRouteA, from: null })
+  const routerRoute: RouterRoute = {
+    matched: route,
+    matches: [route],
+    name: route.name,
+    query: createRouterRouteQuery(),
+    params: {},
+  }
+
+  const execute = (): Promise<void> => executeMiddleware({ to: routerRoute, from: null })
 
   await expect(() => execute()).rejects.toThrowError(error)
 })
@@ -58,17 +77,23 @@ test('middleware is called in order', () => {
   const middlewareA = vi.fn()
   const middlewareB = vi.fn()
   const middlewareC = vi.fn()
-  const routeA = {
+
+  const route = {
     name: 'routeA',
     path: '/routeA',
     component,
     middleware: [middlewareA, middlewareB, middlewareC],
   } as const satisfies Route
 
-  const resolved = resolveRoutes([routeA])
-  const routerRouteA = getRouterRouteForUrl(resolved, routeA.path)!
+  const routerRoute: RouterRoute = {
+    matched: route,
+    matches: [route],
+    name: route.name,
+    query: createRouterRouteQuery(),
+    params: {},
+  }
 
-  executeMiddleware({ to: routerRouteA, from: null })
+  executeMiddleware({ to: routerRoute, from: null })
   const [orderA] = middlewareA.mock.invocationCallOrder
   const [orderB] = middlewareB.mock.invocationCallOrder
   const [orderC] = middlewareC.mock.invocationCallOrder
