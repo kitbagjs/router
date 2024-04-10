c4a2b4c5318e0422509c179128f9d9f306a9c8f8
Craig Harshbarger <pleek91@gmail.com>
2024-04-09 21:10:17 -0500

    Fix indentation

--- src/types/routerRoute.spec-d.ts
+++ src/types/routerRoute.spec-d.ts
@@ -4,11 +4,11 @@ import { Path, Query } from '@/utilities'
 
 describe('ExtractRouterRouteParamTypes', () => {
   test('given routes with different params, some optional, combines into expected args for developer', () => {
-  type Route = RouterRoute<'parentA', Path<'/:paramA', {}>, Query<'foo=:paramB&bar=:?paramC', { paramB: BooleanConstructor }>>
+    type Route = RouterRoute<'parentA', Path<'/:paramA', {}>, Query<'foo=:paramB&bar=:?paramC', { paramB: BooleanConstructor }>>
 
-  type Source = ExtractRouterRouteParamTypes<Route>
-  type Expect = { paramA: string, paramB: boolean, paramC?: string }
+    type Source = ExtractRouterRouteParamTypes<Route>
+    type Expect = { paramA: string, paramB: boolean, paramC?: string }
 
-  expectTypeOf<Source>().toEqualTypeOf<Expect>()
+    expectTypeOf<Source>().toEqualTypeOf<Expect>()
   })
 })
