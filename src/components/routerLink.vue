4328f3e7ef00c8b2736733e5fffa601d5ac8a7c9
Craig Harshbarger <pleek91@gmail.com>
2024-04-19 21:32:07 -0500

    Update router link to use resolve directly

--- src/components/routerLink.vue
+++ src/components/routerLink.vue
@@ -4,25 +4,15 @@
   </a>
 </template>
 
-<script setup lang="ts" generic="T extends RegisteredRoutesKey | Url">
+<script setup lang="ts">
   import { computed, readonly } from 'vue'
   import { useRouter } from '@/compositions'
-  import { RegisteredRoutes, RegisteredRoutesKey } from '@/types/register'
-  import { RouterPush, RouterPushOptions } from '@/types/routerPush'
-  import { RouteParamsByName } from '@/types/routeWithParams'
-  import { Url } from '@/types/url'
-  import { AllPropertiesAreOptional } from '@/types/utilities'
-  import { RouterResolve } from '@/utilities'
+  import { RegisteredRouter } from '@/types/register'
+  import { RouterPushOptions } from '@/types/routerPush'
 
-  type PushMethodArgs<
-    TSource extends RegisteredRoutesKey,
-    TParams = RouteParamsByName<RegisteredRoutes, TSource>
-  > = AllPropertiesAreOptional<TParams> extends true
-    ? [params?: TParams]
-    : [params: TParams]
+  type ToCallback = (resolve: RegisteredRouter['resolve']) => string
 
-  type PushMethod = <T extends <TSource extends RegisteredRoutesKey>(source: TSource, ...args: PushMethodArgs<TSource>) => any>(push: T) => ReturnType<T>
-  const props = defineProps<{ to: string | PushMethod } & RouterPushOptions>()
+  const props = defineProps<{ to: string | ToCallback } & RouterPushOptions>()
 
   defineSlots<{
     default?: (props: {
@@ -35,13 +25,18 @@
 
   const router = useRouter()
 
-  const route = computed(() => {
-    const { to, ...options } = props
-    if (typeof to === 'string') {
-      return router.find(to as any, options)?.matched
-    }
+  const resolved = computed(() => {
+    return typeof props.to === 'string' ? props.to : props.to(router.resolve)
+  })
 
-    return to(router.find)?.matched
+  const options = computed(() => {
+    const { to, ...options } = props
+
+    return options
+  })
+
+  const route = computed(() => {
+    return router.find(resolved.value, options.value)?.matched
   })
 
   const match = computed(() => !!route.value && router.route.matches.includes(readonly(route.value)))
@@ -52,34 +47,15 @@
     'router-link--exact-match': exactMatch.value,
   }))
 
-  const resolved = computed(() => {
-    const { to, ...options } = props
-    if (typeof to === 'string') {
-      return router.resolve(to as any, options)
-    }
-
-    const optionsBound: RouterResolve<RegisteredRoutes> = (source: any, params: any) => router.resolve(source, params, options)
-
-    return to(optionsBound)
-  })
-  const host = computed(() => {
+  const isExternal = computed(() => {
     const { host } = new URL(resolved.value, window.location.origin)
 
-    return host
+    return host !== window.location.host
   })
-  const isExternal = computed(() => host.value !== window.location.host)
 
   function onClick(event: MouseEvent): void {
     event.preventDefault()
 
-    const { to, ...options } = props
-    if (typeof to === 'string') {
-      router.push(to as any, options)
-      return
-    }
-
-    const optionsBound: RouterPush<RegisteredRoutes> = (source: any, params: any) => router.push(source, params, options)
-
-    to(optionsBound)
+    router.push(resolved.value, options.value)
   }
 </script>
