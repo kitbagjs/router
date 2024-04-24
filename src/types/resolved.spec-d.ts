36f01e0841797e8de8a7c2b65dcdd57c695de1f0
Evan Sutherland <stackoverfloweth@gmail.com>
2024-04-23 20:50:36 -0500

    code review suggestions

--- src/types/resolved.spec-d.ts
+++ src/types/resolved.spec-d.ts
@@ -1,20 +1,20 @@
 import { expectTypeOf, test } from 'vitest'
-import { ResolvedRoute } from '@/types/resolved'
+import { RouterRoute } from '@/types/resolved'
 import { Route } from '@/types/route'
 import { Path, Query } from '@/utilities'
 
 test('given a specific Route, params are narrow', () => {
     type TestRoute = Route<'parentA', Path<'/:paramA', {}>, Query<'foo=:paramB&bar=:?paramC', { paramB: BooleanConstructor }>>
 
-    type Source = ResolvedRoute<TestRoute>['params']
+    type Source = RouterRoute<TestRoute>['params']
     type Expect = { paramA: string, paramB: boolean, paramC?: string | undefined }
 
     expectTypeOf<Expect>().toMatchTypeOf<Source>()
 })
 
 test('without a specific Route, params are Record<string, unknown>', () => {
-  type Source = ResolvedRoute['params']
-  type Expect = Record<string, Readonly<unknown>>
+  type Source = RouterRoute['params']
+  type Expect = Record<string, unknown>
 
   expectTypeOf<Expect>().toMatchTypeOf<Source>()
 })
