51e6f91bd4915fe519cb977011a7404950c5f1ba
Evan Sutherland <stackoverfloweth@gmail.com>
2024-04-27 08:17:07 -0500

    fix type mismatch on router.update

--- src/types/routerUpdate.ts
+++ src/types/routerUpdate.ts
@@ -1,5 +1,4 @@
 import { Param } from '@/types/params'
-import { ResolvedRoute } from '@/types/resolved'
 import { ExtractRouteParamTypes, Route } from '@/types/route'
 
 export type RouteUpdateOptions = {
@@ -7,10 +6,7 @@ export type RouteUpdateOptions = {
   replace?: boolean,
 }
 
-export type RouterUpdate = {
-  <TRoute extends ResolvedRoute, TKey extends keyof TRoute['params']>(route: TRoute, paramKey: TKey, paramValue: TRoute['params'][TKey], options?: RouteUpdateOptions): Promise<void>,
-  <TRoute extends ResolvedRoute>(route: TRoute, params: Partial<TRoute['params']>, options?: RouteUpdateOptions): Promise<void>,
-}
+export type RouterUpdate = (params: Partial<Record<string, unknown>>, options?: RouteUpdateOptions) => Promise<void>
 
 export type RouteUpdate<TRoute extends Route = Route, TParams extends Record<string, Param | undefined> = ExtractRouteParamTypes<TRoute>> = {
   <TKey extends keyof TParams>(key: TKey, value: TParams[TKey], options?: RouteUpdateOptions): Promise<void>,
