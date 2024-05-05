c43f70578f5086097df63a17cf85995f89176422
Evan Sutherland <stackoverfloweth@gmail.com>
2024-05-05 12:46:38 -0500

    Update src/errors/invalidRouteParamValueError.ts
    Co-authored-by: Craig Harshbarger <pleek91@gmail.com>

--- src/errors/invalidRouteParamValueError.ts
+++ src/errors/invalidRouteParamValueError.ts
@@ -1,5 +1,5 @@
 /**
- * Represents an error thrown when an invalid value is assigned to a route parameter.
+ * An error thrown when an invalid value is assigned to a route parameter.
  * This error indicates a mismatch of param value provided in the URL and the expected format for the param.
  * For example if a param is type Boolean but the URL provides a non-boolean value such as "ABC".
  * Custom Param types are given the string value from the URL as well as an `invalid` function.
