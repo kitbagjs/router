a7892165e950b9331d709d33f75a2009b2effd82
Evan Sutherland <stackoverfloweth@gmail.com>
2024-05-07 21:09:21 -0500

    removed jsdoc strings for non-public error classes

--- src/errors/routerRejectionError.ts
+++ src/errors/routerRejectionError.ts
@@ -1,8 +1,5 @@
 import { RouterRejectionType } from '@/services/createRouterReject'
 
-/**
- * Represents an error thrown when a routing action is explicitly rejected, categorized by a specific rejection type.
- */
 export class RouterRejectionError extends Error {
   public type: RouterRejectionType
 
