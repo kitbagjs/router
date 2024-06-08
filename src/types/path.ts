08ffbee5715723a51d77f0a7b1d67acf7e792d13
Evan Sutherland <stackoverfloweth@gmail.com>
2024-06-08 15:26:12 -0500

    path and query types have stronger typed defaults, when used without defaults will have `Record<string, Param>` for `params`

--- src/types/path.ts
+++ src/types/path.ts
@@ -16,11 +16,11 @@ export type PathParams<TPath extends string> = {
 }
 
 export type Path<
-  TPath extends string = any,
-  TParams extends PathParams<TPath> = any
+  TPath extends string = string,
+  TParams extends PathParams<TPath> = Record<string, Param | undefined>
 > = {
   path: TPath,
-  params: Identity<ExtractParamsFromPathString<TPath, TParams>>,
+  params: string extends TPath ? Record<string, Param> : Identity<ExtractParamsFromPathString<TPath, TParams>>,
   toString: () => string,
 }
 export type ToPath<T extends string | Path> = T extends string ? Path<T, {}> : T
