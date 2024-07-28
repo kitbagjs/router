772a00aeb444ec9b86f1b2830b9d7fbc7207b9a1
Evan Sutherland <stackoverfloweth@gmail.com>
2024-07-28 08:33:39 -0500

    combine register interface and meta, case insensitive

--- docs/api/interfaces/Register.md
+++ docs/api/interfaces/Register.md
@@ -1,14 +1,28 @@
 # Register
 
-Represents the state of currently registered router, and rejections. Used to provide correct type context for
+Represents the state of currently registered router, and rejections.
+
+## router
+
+Used to provide correct type context for
 components like `RouterLink`, as well as for composables like `useRouter`, `useRoute`, and hooks.
+
+## rejections
+
+Used to extend the default rejections of ['NotFound'] to include your custom rejections. Note that the default "NotFound" is always an available rejection.
+
+## routeMeta
+
+This type will be used as a default on `meta` when defining your route with `createRoute`.
 
 ## Example
 
 ```ts
 declare module '@kitbag/router' {
   interface Register {
-    router: typeof router
+    router: typeof router,
+    rejections: ["NotAuthorized"],
+    routeMeta: { public?: boolean }
   }
 }
 ```
--- src/types/register.ts
+++ src/types/register.ts
@@ -5,7 +5,7 @@ import { RouterReplace } from '@/types/routerReplace'
 import { RoutesKey, RoutesMap } from '@/types/routesMap'
 
 /**
- * Represents the state of currently registered router, and rejections. Used to provide correct type context for
+ * Represents the state of currently registered router, rejections, and route meta. Used to provide correct type context for
  * components like `RouterLink`, as well as for composables like `useRouter`, `useRoute`, and hooks.
  *
  * @example
@@ -13,6 +13,8 @@ import { RoutesKey, RoutesMap } from '@/types/routesMap'
  * declare module '@kitbag/router' {
  *   interface Register {
  *     router: typeof router
+ *     rejections: ["NotAuthorized"],
+ *     routeMeta: { public?: boolean }
  *   }
  * }
  * ```
@@ -24,33 +26,41 @@ export interface Register {}
  */
 export type RegisteredRouter = Register extends { router: infer TRouter }
   ? TRouter
-  : Router
-
-/**
- * Represents the Router property within {@link Register}
- */
-export type RegisteredRouterRoute = RegisteredRouter['route']
+  : Register extends { Router: infer TRouter }
+    ? TRouter
+    : Router
 
 /**
  * Represents the Router routes property within {@link Register}
  */
 export type RegisteredRoutes = Register extends { router: Router<infer TRoutes extends Routes> }
   ? TRoutes
-  : Route[]
+  : Register extends { Router: Router<infer TRoutes extends Routes> }
+    ? TRoutes
+    : Route[]
 
 /**
  * Represents the possible Rejections registered within {@link Register}
  */
 export type RegisteredRejectionType = Register extends { rejections: infer TRejections extends string[] }
   ? TRejections[number]
-  : never
+  : Register extends { Rejections: infer TRejections extends string[] }
+    ? TRejections
+    : never
 
 /**
- * Represents the State property registered within {@link Register}
+ * Represents additional metadata associated with a route, customizable via declaration merging.
  */
-export type RegisteredRouterState = Register extends { state: infer TState }
-  ? TState
-  : {}
+export type RouteMeta = Register extends { routeMeta: infer RouteMeta extends Record<string, unknown> }
+  ? RouteMeta
+  : Register extends { RouteMeta: infer RouteMeta extends Record<string, unknown> }
+    ? RouteMeta
+    : Record<string, unknown>
+
+/**
+ * Represents the Router property within {@link Register}
+ */
+export type RegisteredRouterRoute = RegisteredRouter['route']
 
 /**
  * Represents the a map of all possible RouteKeys with corresponding Route registered within {@link Register}
@@ -71,16 +81,3 @@ export type RegisteredRouterPush = RouterPush<RegisteredRoutes>
  * Represents the type for router `replace`, with types for routes registered within {@link Register}
  */
 export type RegisteredRouterReplace = RouterReplace<RegisteredRoutes>
-
-/**
- * Represents additional metadata associated with a route, customizable via declaration merging.
- * @example
- * ```ts
- * declare module '@kitbag/router' {
- *   interface RouteMeta {
- *     pageTitle?: string
- *   }
- * }
- * ```
- */
-export interface RouteMeta extends Record<string, unknown> {}
