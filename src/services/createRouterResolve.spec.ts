e4e7fa5d312c6439dc0ed6730acf51794162b7ae
Evan Sutherland <stackoverfloweth@gmail.com>
2024-11-09 14:32:11 -0600

    updated tests that throw to use specific error name

--- src/services/createRouterResolve.spec.ts
+++ src/services/createRouterResolve.spec.ts
@@ -3,6 +3,7 @@ import { createExternalRoute } from '@/services/createExternalRoute'
 import { createRoute } from '@/services/createRoute'
 import { createRouterResolve } from '@/services/createRouterResolve'
 import { component, routes } from '@/utilities/testHelpers'
+import { RouteNotFoundError } from '@/errors/routeNotFoundError'
 
 test('given a url returns that string', () => {
   const resolve = createRouterResolve(routes)
@@ -23,11 +24,13 @@ test('given a route name with query, interpolates param values', () => {
   expect(url).toBe('/parentA/bar?foo=foo')
 })
 
-test('given a route name with params cannot be matched, throws an error', () => {
+test('given a route name with params cannot be matched, throws an RouteNotFoundError', () => {
   const resolve = createRouterResolve(routes)
 
   // @ts-expect-error route doesn't actually exist
-  expect(() => resolve({ route: 'foo' })).toThrowError()
+  const action: () => void = () => resolve({ route: 'foo' })
+
+  expect(action).toThrow(RouteNotFoundError)
 })
 
 test('given a param with a dash or underscore resolves the correct url', () => {
