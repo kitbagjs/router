676bc9700bfa2c551367b303183df1186d17fe48
Evan Sutherland <stackoverfloweth@gmail.com>
2024-12-17 11:18:40 -0600

    fixing test

--- src/services/state.ts
+++ src/services/state.ts
@@ -14,11 +14,16 @@ export function getStateValue(state: unknown, key: string, param: Param): unknow
     if (typeof value === 'string') {
       return getParamValue(value, param, isOptional)
     }
+
+    return value
   }
 
   return getParamValue(undefined, param, isOptional)
 }
 
+/**
+ * This function is used to get the values inside the state converted from string values into the correct type.
+ */
 export function getStateValues(params: Record<string, Param>, state: unknown): Record<string, unknown> {
   const values: Record<string, unknown> = {}
 
@@ -31,6 +36,9 @@ export function getStateValues(params: Record<string, Param>, state: unknown): R
   return values
 }
 
+/**
+ * This function is used to set the values inside the state to have string values, stored in history.
+ */
 export function setStateValue(state: unknown, key: string, param: Param): string | undefined {
   if (stateIsRecord(state) && key in state) {
     const value = state[key]
