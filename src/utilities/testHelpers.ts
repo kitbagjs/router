15696a6fa26c9fec8c2f184aa4f77e6cc3820ac3
Evan Sutherland <stackoverfloweth@gmail.com>
2025-01-07 21:19:32 -0600

    face_palm

--- src/utilities/testHelpers.ts
+++ src/utilities/testHelpers.ts
@@ -30,8 +30,6 @@ const parentA = createRoute({
   path: '/parentA/[paramA]',
 })
 
-export type T1 = typeof parentA['path']
-
 const childA = createRoute({
   parent: parentA,
   name: 'parentA.childA',
