dd6e409fabccc7343d89fd719f9ff80c8a2c25bc
Evan Sutherland <stackoverfloweth@gmail.com>
2025-01-26 17:09:41 -0600

    update exports, backwards compatible

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,10 +1,21 @@
 export * from './createParam'
 export * from './createRouter'
-export * from './path'
-export * from './query'
+export { withParams } from './withParams'
 export type { RouterRoute } from './createRouterRoute'
 export { withDefault } from './withDefault'
 export { createRouterPlugin } from './createRouterPlugin'
 export { unionOf } from './unionOf'
 export { arrayOf } from './arrayOf'
 export { tupleOf } from './tupleOf'
+
+// backwards compatible withParams
+import { withParams, WithParams, toWithParams } from './withParams'
+export const toPath = toWithParams
+export const path = withParams
+export type Path = WithParams
+export const toHost = toWithParams
+export const host = withParams
+export type Host = WithParams
+export const toQuery = toWithParams
+export const query = withParams
+export type Query = WithParams
