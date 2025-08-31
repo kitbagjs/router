b5b23eec3a4fd97b1de19b77ad507a794d4c79e2
Craig Harshbarger <pleek91@gmail.com>
2025-08-30 21:53:16 -0500

    tweak

--- src/compositions/useRouter.ts
+++ src/compositions/useRouter.ts
@@ -4,16 +4,6 @@ import { RegisteredRouter, Router } from '@/types'
 
 export const routerInjectionKey: InjectionKey<RegisteredRouter> = Symbol()
 
-/**
- * A composition to access the registered router instance within a Vue component.
- *
- * @returns The registered router instance.
- * @throws {RouterNotInstalledError} Throws an error if the router has not been installed,
- *         ensuring the component does not operate without routing functionality.
- * @group Compositions
- */
-export const useRouter = createUseRouter(routerInjectionKey)
-
 export function createUseRouter<T extends Router>(key: InjectionKey<T>): () => T {
   return () => {
     const router = inject(key)
@@ -25,3 +15,13 @@ export function createUseRouter<T extends Router>(key: InjectionKey<T>): () => T
     return router
   }
 }
+
+/**
+ * A composition to access the registered router instance within a Vue component.
+ *
+ * @returns The registered router instance.
+ * @throws {RouterNotInstalledError} Throws an error if the router has not been installed,
+ *         ensuring the component does not operate without routing functionality.
+ * @group Compositions
+ */
+export const useRouter = createUseRouter(routerInjectionKey)
