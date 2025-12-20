381149c6f247b9c658f4c54b0b96533176bdd273
Evan Sutherland <stackoverfloweth@gmail.com>
2025-12-20 13:39:47 -0600

    remove mock test utilities

--- src/services/getRouteHooks.spec.ts
+++ src/services/getRouteHooks.spec.ts
@@ -1,21 +1,20 @@
 import { expect, test } from 'vitest'
 import { getBeforeRouteHooksFromRoutes } from '@/services/getRouteHooks'
-import { mockResolvedRoute, mockRoute } from '@/utilities/testHelpers'
-import { asArray } from '@/utilities/array'
+import { createRoute } from './createRoute'
+import { createResolvedRoute } from './createResolvedRoute'
 
 test('given two ResolvedRoutes returns before timing hooks in correct order', () => {
-  const grandchildA = mockRoute('grandchildA')
-  const grandchildB = mockRoute('grandchildB')
-  const childA = mockRoute('childA')
-  const childB = mockRoute('childB')
-  const parent = mockRoute('parentA')
+  const parent = createRoute({ name: 'parentA' })
+  const childA = createRoute({ name: 'childA', parent })
+  const grandchildA = createRoute({ name: 'grandchildA', parent: childA })
+  const childB = createRoute({ name: 'childB', parent })
+  const grandchildB = createRoute({ name: 'grandchildB', parent: childA })
 
-  const to = mockResolvedRoute(grandchildB, [parent, childA, grandchildA])
-  const from = mockResolvedRoute(grandchildA, [parent, childB, grandchildB])
+  const to = createResolvedRoute(grandchildB, {})
+  const from = createResolvedRoute(grandchildA, {})
 
   const hooks = getBeforeRouteHooksFromRoutes(to, from)
-
-  expect(Array.from(hooks.onBeforeRouteEnter)).toMatchObject([...asArray(childA.onBeforeRouteEnter ?? []), ...asArray(grandchildA.onBeforeRouteEnter ?? [])])
-  expect(Array.from(hooks.onBeforeRouteUpdate)).toMatchObject([...asArray(parent.onBeforeRouteUpdate ?? [])])
-  expect(Array.from(hooks.onBeforeRouteLeave)).toMatchObject([...asArray(childB.onBeforeRouteLeave ?? []), ...asArray(grandchildB.onBeforeRouteLeave ?? [])])
+  expect(Array.from(hooks.onBeforeRouteEnter)).toMatchObject([...Array.from(childA.stores.at(0)?.onBeforeRouteEnter ?? []), ...Array.from(grandchildA.stores.at(0)?.onBeforeRouteEnter ?? [])])
+  expect(Array.from(hooks.onBeforeRouteUpdate)).toMatchObject([...Array.from(parent.stores.at(0)?.onBeforeRouteUpdate ?? [])])
+  expect(Array.from(hooks.onBeforeRouteLeave)).toMatchObject([...Array.from(childB.stores.at(0)?.onBeforeRouteLeave ?? []), ...Array.from(grandchildB.stores.at(0)?.onBeforeRouteLeave ?? [])])
 })
