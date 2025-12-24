2eebd4f4475113b8a690572b8d1d60ba6590166c
Evan Sutherland <stackoverfloweth@gmail.com>
2025-12-24 09:32:30 -0600

    fixed code suggestion

--- src/utilities/checkMissingContext.ts
+++ src/utilities/checkMissingContext.ts
@@ -3,13 +3,13 @@ import { Route, Routes } from '@/types/route'
 import { RouteContext } from '@/types/routeContext'
 
 export function checkMissingContext(routes: Routes): void {
-  const routeIds = new Set(routes.map(route => route.id)
+  const routeIds = new Set(routes.map((route) => route.id))
   const routeIdsFromContext = routes
     .flatMap((route) => route.context)
     .filter(contextIsRoute)
 
   for (const route of routeIdsFromContext) {
-    if (!routeIds.has(route.id) {
+    if (!routeIds.has(route.id)) {
       throw new MissingRouteContextError(route.name)
     }
   }
