f9079bcc4bd7df9d1f370f1d319b24c9b24a7775
Evan Sutherland <stackoverfloweth@gmail.com>
2026-01-14 21:33:17 -0600

    revert params changes in favor of a simpler approach

--- src/types/url.ts
+++ src/types/url.ts
@@ -1,5 +1,6 @@
+/* eslint-disable @typescript-eslint/method-signature-style */
 import { WithParams } from '@/services/withParams'
-import { ExtractRecordParamTypesReading, ExtractRecordParamTypesWriting } from '@/types/params'
+import { ExtractUrlParamTypesReading, ExtractUrlParamTypesWriting } from '@/types/params'
 import { AllPropertiesAreOptional } from '@/types/utilities'
 import { UrlString } from '@/types/urlString'
 
@@ -16,40 +17,43 @@ export type Url<
    * Represents the host for this route. Used for external routes.
   */
   host: THost & {
-    toString: (...params: UrlParamsArgs<{ host: THost }>) => string,
+    toString(...params: UrlParamsArgs<{ host: THost }>): string,
   },
   /**
    * Represents the structured path of the route, including path params.
   */
   path: TPath & {
-    toString: (...params: UrlParamsArgs<{ path: TPath }>) => string,
+    toString(...params: UrlParamsArgs<{ path: TPath }>): string,
   },
   /**
    * Represents the structured query of the route, including query params.
   */
   query: TQuery & {
-    toString: (...params: UrlParamsArgs<{ query: TQuery }>) => string,
+    toString(...params: UrlParamsArgs<{ query: TQuery }>): string,
   },
   /**
    * Represents the hash of the route.
    */
   hash: THash & {
-    toString: (...params: UrlParamsArgs<{ hash: THash }>) => string,
+    toString(...params: UrlParamsArgs<{ hash: THash }>): string,
   },
   /**
    * Converts the url parts to a full url.
    */
-  toString: (...params: UrlParamsArgs<{ host: THost, path: TPath, query: TQuery, hash: THash }>) => UrlString,
+  toString(...params: UrlParamsArgs<{ host: THost, path: TPath, query: TQuery, hash: THash }>): UrlString,
   /**
    * Parses the url supplied and returns any params found.
    */
-  parse: (url: string) => ExtractRecordParamTypesReading<{ host: THost, path: TPath, query: TQuery, hash: THash }>,
+  parse(url: string): ExtractUrlParamTypesReading<{ host: THost, path: TPath, query: TQuery, hash: THash }>,
 }
 
 type UrlParamsArgs<
-  TParts extends Record<string, unknown> = Record<string, unknown>
-> = ExtractRecordParamTypesWriting<TParts> extends Record<string, unknown>
-  ? [params?: Record<string, unknown>] // generic URL without generics uses this
-  : AllPropertiesAreOptional<ExtractRecordParamTypesWriting<TParts>> extends true
-    ? [params?: ExtractRecordParamTypesWriting<TParts>]
-    : [params: ExtractRecordParamTypesWriting<TParts>]
+  TUrl extends {
+    host?: WithParams,
+    path?: WithParams,
+    query?: WithParams,
+    hash?: WithParams,
+  }
+> = AllPropertiesAreOptional<ExtractUrlParamTypesWriting<TUrl>> extends true
+  ? [params?: ExtractUrlParamTypesWriting<TUrl>]
+  : [params: ExtractUrlParamTypesWriting<TUrl>]
