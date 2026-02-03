f412eb7e0ca04e7b54375d7368a8d2b04b3e916c
Evan Sutherland <stackoverfloweth@gmail.com>
2026-02-03 14:54:37 -0600

    make trailing slashes optional for all routes by default

--- src/services/routeRegex.ts
+++ src/services/routeRegex.ts
@@ -53,8 +53,10 @@ export function generateRouteHostRegexPattern(host: string): RegExp {
 
 export function generateRoutePathRegexPattern(path: string): RegExp {
   const pathRegex = replaceParamSyntaxWithCatchAllsAndEscapeRest(path)
+  const hasTrailingSlash = path.endsWith('/')
+  const normalizedRegex = hasTrailingSlash ? pathRegex.replace(/\/$/, '') + '\\/?' : pathRegex
 
-  return new RegExp(`^${pathRegex}$`, 'i')
+  return new RegExp(`^${normalizedRegex}$`, 'i')
 }
 
 export function generateRouteHashRegexPattern(hash: string): RegExp {
