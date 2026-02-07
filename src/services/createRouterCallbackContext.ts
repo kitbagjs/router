c37ee5d676ca758ef995569be85c2572b5c28bff
Evan Sutherland <stackoverfloweth@gmail.com>
2026-02-07 14:29:41 -0600

    clean unused exports

--- src/services/createRouterCallbackContext.ts
+++ src/services/createRouterCallbackContext.ts
@@ -5,49 +5,17 @@ import { RouterPush, RouterPushOptions } from '@/types/routerPush'
 import { RouterReject } from '@/types/routerReject'
 import { RouterReplace } from '@/types/routerReplace'
 import { isUrlString } from '@/types/urlString'
-import { BuiltInRejectionType } from './createRouterReject'
-import { AsString } from '@/types/utilities'
 import { Routes } from '@/types/route'
 import { Rejections } from '@/types/rejection'
 import { RouteUpdate } from '@/types/routeUpdate'
 import { ResolvedRoute } from '@/types/resolved'
 
 /**
- * Defines the structure of a successful callback response.
- */
-export type RouterCallbackSuccessResponse = {
-  status: 'SUCCESS',
-}
-
-/**
- * Defines the structure of an aborted callback response.
- */
-export type RouterCallbackAbortResponse = {
-  status: 'ABORT',
-}
-
-/**
- * Defines the structure of a callback response that results in a push to a new route.
- */
-export type RouterCallbackPushResponse<TRoutes extends Routes> = {
-  status: 'PUSH',
-  to: Parameters<RouterPush<TRoutes>>,
-}
-
-/**
- * Defines the structure of a callback response that results in the rejection of a route transition.
- */
-export type RouterCallbackRejectResponse<TRejections extends string> = {
-  status: 'REJECT',
-  type: AsString<TRejections> | BuiltInRejectionType,
-}
-
-/**
  * A function that can be called to abort a routing operation.
  */
 export type CallbackContextAbort = () => void
 
-export type RouterCallbackContext<
+type RouterCallbackContext<
   TRoutes extends Routes = Routes,
   TRejections extends Rejections = Rejections
 > = {
