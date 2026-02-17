aa26c851fd62448932b4f6c3fbc7745f80912360
Craig Harshbarger <pleek91@gmail.com>
2026-02-16 18:00:54 -0600

    Revert cursor change

--- src/guards/routes.ts
+++ src/guards/routes.ts
@@ -7,9 +7,6 @@ export type IsRouteOptions = {
   exact?: boolean,
 }
 
-/**
- * Filters route union to routes whose match chain contains TRouteName (self or ancestor).
- */
 export type RouteWithMatch<
   TRoute extends RouterRoute,
   TRouteName extends TRoute['name']
@@ -63,7 +60,7 @@ type IsRouteFunction<TRouter extends Router> = {
 }
 
 export function createIsRoute<TRouter extends Router>(routerKey: InjectionKey<TRouter>): IsRouteFunction<TRouter> {
-  return ((route: unknown, routeName?: string, { exact }: IsRouteOptions = {}): route is any => {
+  return (route: unknown, routeName?: string, { exact }: IsRouteOptions = {}): route is any => {
     if (!isRouterRoute(routerKey, route)) {
       return false
     }
@@ -77,5 +74,5 @@ export function createIsRoute<TRouter extends Router>(routerKey: InjectionKey<TR
     }
 
     return route.matches.map((route) => route.name).includes(routeName)
-  }) as IsRouteFunction<TRouter>
+  }
 }
