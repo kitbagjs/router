726a9f13cdd5b484d44f632f32c26054ca393b16
Evan Sutherland <stackoverfloweth@gmail.com>
2026-03-20 15:09:44 -0500

    rename titles to routeTitles

--- src/types/routeTitle.ts
+++ src/types/routeTitle.ts
@@ -2,35 +2,35 @@ import { ResolvedRoute, ResolvedRouteUnion } from '@/types/resolved'
 import { isRoute, Route } from './route'
 import { MaybePromise } from './utilities'
 
-export type SetTitleContext = {
+export type SetRouteTitleContext = {
   from: ResolvedRoute,
   getParentTitle: () => Promise<string | undefined>,
 }
 
-export type SetTitleCallback<TRoute extends Route = Route> = (to: ResolvedRouteUnion<TRoute>, context: SetTitleContext) => MaybePromise<string>
-export type GetTitle<TRoute extends Route = Route> = (to: ResolvedRouteUnion<TRoute>) => Promise<string | undefined>
-export type SetTitle<TRoute extends Route = Route> = (callback: SetTitleCallback<TRoute>) => void
+export type SetRouteTitleCallback<TRoute extends Route = Route> = (to: ResolvedRouteUnion<TRoute>, context: SetRouteTitleContext) => MaybePromise<string>
+export type GetRouteTitle<TRoute extends Route = Route> = (to: ResolvedRouteUnion<TRoute>) => Promise<string | undefined>
+export type SetRouteTitle<TRoute extends Route = Route> = (callback: SetRouteTitleCallback<TRoute>) => void
 
 export type RouteSetTitle<TRoute extends Route = Route> = {
   /**
    * Adds a callback to set the document title for the route.
    */
-  setTitle: SetTitle<TRoute>,
+  setTitle: SetRouteTitle<TRoute>,
 }
 
 type CreateRouteTitle = {
-  setTitle: SetTitle,
-  getTitle: GetTitle,
+  setTitle: SetRouteTitle,
+  getTitle: GetRouteTitle,
 }
 
 export function createRouteTitle(parent: Route | undefined): CreateRouteTitle {
-  let setTitleCallback: SetTitleCallback | undefined
+  let setTitleCallback: SetRouteTitleCallback | undefined
 
-  const setTitle: SetTitle = (callback) => {
+  const setTitle: SetRouteTitle = (callback) => {
     setTitleCallback = callback
   }
 
-  const getTitle: GetTitle = async (to) => {
+  const getTitle: GetRouteTitle = async (to) => {
     const getParentTitle = async (): Promise<string | undefined> => {
       if (parent && isRoute(parent)) {
         return parent.getTitle(to)
