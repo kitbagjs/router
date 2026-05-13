4c60c7d2a315e6274d1fdafa2e82f4c6baf8587f
Evan Sutherland <stackoverfloweth@gmail.com>
2026-05-13 09:28:42 -0500

    drop defaultTitle from setDocumentTitle logic

--- src/utilities/setDocumentTitle.ts
+++ src/utilities/setDocumentTitle.ts
@@ -2,16 +2,16 @@ import { ResolvedRoute } from '@/types/resolved'
 import { isRoute } from '@/types/route'
 import { isBrowser } from '@/utilities/isBrowser'
 
-let defaultTitle: string | undefined
-
 export function setDocumentTitle(to: ResolvedRoute | null): void {
   if (!isRoute(to) || !isBrowser()) {
     return
   }
 
-  defaultTitle ??= document.title
-
   to.title.then((value) => {
-    document.title = value ?? defaultTitle ?? ''
+    if (value === undefined) {
+      return
+    }
+
+    document.title = value
   })
 }
